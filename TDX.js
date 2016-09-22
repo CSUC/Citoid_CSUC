3d7750b0c9aaae14fadba9301777437df328c0d8
Joan Caparrós <joan.caparros@csuc.cat>
2016-09-22 13:34:41 +0200

    Update TDX.js

--- TDX.js
+++ TDX.js
@@ -1,61 +1,44 @@
-{
-	"translatorID": "",
-	"label": "TDX",
-	"creator": "CSUC",
-	"target": "^http://www\\.tdx\\.cat/",
-	"minVersion": "3",
-	"maxVersion": "",
-	"priority": 100,
-	"inRepository": true,
-	"translatorType": 4,
-	"browserSupport": "gcsv",
-	"lastUpdated": "2016-09-21 12:10:00"
-}
-
-
 function detectWeb(doc, url) {
-	var type = ZU.xpath(doc, '//meta[contains(@name, "DC.type")]/@content');
+	var type = ZU.xpath(doc, '//meta[@name="DC.type"]/@content');
 	if (type.length>0) {
 		if (mappingTable[type[0].textContent]) {
 			return mappingTable[type[0].textContent];
 		} else {
 			return "doctoralThesis";
 		}
-	}
-	
-	if ( getSearchResults(doc).length>0 ) {
+	}else{
 		return "multiple";
 	}
 }
 
 var mappingTable = {
-	"monograph" : "book",
-	"article" : "journalArticle",
-	"collection" : "book",
-	"incollection" : "bookSection",
-	"recension" : "journalArticle",
-	"techreport" : "report",//not yet used
-	"inproceeding" : "conferencePaper",//not yet used
+	"info:eu-repo/semantics/doctoralThesis":"doctoralThesis",
 }
 
-function getSearchResults(doc, checkOnly) {
-	var items = {};
-	var found = false;
-	// TO DO
-	return found ? items : false;
-}
+
+
 
 function doWeb(doc, url) {
 	if (detectWeb(doc, url) == "multiple") {
-		Zotero.selectItems(getSearchResults(doc, false), function (items) {
+		
+				var hits=[];
+	var urls=[];
+	var results=ZU.xpath(doc,'//div[@class="artifact-title"]/a');
+	for (var i in results) {
+		hits.push(results[i].href);
+
+			}
+			
+		Zotero.selectItems(hits, function (items) {
 			if (!items) {
 				return true;
 			}
 			var items = new Array();
-			for (var i in items) {
-				items;
+			for (var j in items) {
+				urls.push(j);
+				
 			}
-			ZU.processDocuments(items, scrape);
+			ZU.processDocuments(urls, scrape);
 		});
 	} else {
 		scrape(doc, url);
@@ -63,59 +46,90 @@ function doWeb(doc, url) {
 }
 
 function scrape(doc, url) {
-			
+			var item = new Zotero.Item("thesis");
 			//title
-			var titles = ZU.xpath(doc, '//meta[contains(@name, "DC.title")]');
-			if (!item.DOI) {
-				for (var i=0; i<titles.length; i++) {
-					item.title = item.title[0] + item.title.substr(1).toLowerCase();
-				}
+			var titles = ZU.xpath(doc, '//meta[@name="citation_title"]');
+			if (titles) {
+					item.title = titles[0].content
+			}
+			
+			//add isbn
+			var isbn = ZU.xpath(doc, '//meta[@name="citation_isbn"]');
+			if (isbn.length>0) {
+					item.isbn = isbn[0].content
+			}
+			
+			//add issn
+			var issn = ZU.xpath(doc, '//meta[@name="citation_issn"]');
+			if (issn.length>0) {
+					item.issn = issn[0].content
+			}
+			
+			//add DATE
+			var date = ZU.xpath(doc, '//meta[@name="citation_date"]');
+			if (date.length>0) {
+					item.Date = date[0].content
+			}
+			
+			//add abstract
+			var abstract = ZU.xpath(doc, '//meta[@name="citation_abstract"]');
+			if (abstract.length>0) {
+					item.abstract = abstract[0].content
 			}
 			
 			//add DOI
 			var identfiers = ZU.xpath(doc, '//meta[contains(@name, "DC.identifier")]');
-			if (!item.DOI) {
-				for (var i=0; i<identfiers.length; i++) {
-					item.DOI = identfiers[i].getAttribute("content");
-				}
+			if (identfiers.length>0) {
+					item.url=identfiers[0].content;
 			}
 			
 			//add language
-			item.language = ZU.xpathText(doc, '//meta[contains(@name, "DC.language")]/@content');
+			var language = ZU.xpath(doc, '//meta[@name="citation_language"]');
+			if (language.length>0) {
+					item.language=language[0].content;
+			}
+
+			//add publisher 
+			var publisher = ZU.xpath(doc, '//meta[@name="citation_publisher"]');
+			if (publisher.length>0) {
+				for (var i=0; i<publisher.length; i++) {
+					item.publisher = publisher[i].content;
+				}
+				
+			}
 			
-			
-			var authors = ZU.xpath(doc, '//meta[@name="DC.creator" and @content]|//meta[@name="DC.contributor" and @content]');
+			var authors = ZU.xpath(doc, '//meta[@name="citation_author"]');
 			if (authors) {
 				item.creators = [];
 				for (var a=0; a<authors.length; a++) {
 					var authorsText = authors[a].content;
 				var authorsType = authors[a].name.substr(3);//either creator or contributer
-				var authorParts = authorsText.split(' ');
+				var authorParts = authorsText.split(',');
 				//distinguish between lastName (every letter is in uppercase) from firstName
 				//but there might also be just initials (e.g. "D.") from the firstName
 				var firstName = "";
 				var lastName = "";
 				var splitPos=0;
-				while (splitPos<authorParts.length && authorParts[splitPos].toUpperCase() == authorParts[splitPos] && authorParts[splitPos].indexOf('.') == -1 && authorParts[splitPos].length>1) {
-					authorParts[splitPos] = ZU.capitalizeTitle(authorParts[splitPos], true);
-					splitPos++;
-				}
-				if (splitPos == authorParts.length && splitPos>1) {//guess: last part is firstName
-					firstName = authorParts[splitPos-1];
-					lastName = authorParts.slice(0,splitPos-1).join(' ')
-				} else {
-					firstName = authorParts.slice(splitPos).join(' ');
-					lastName= authorParts.slice(0,splitPos).join(' ');
-				}
-				if (authorsType == "creator") {
-					item.creators.push( {lastName:lastName.trim(), firstName:firstName.trim(), creatorType:"author" });
-				} else {
-					item.creators.push( {lastName:lastName.trim(), firstName:firstName.trim(), creatorType:"contributor" });
-				}
 				
+				if (authorParts.length>1){
+				firstName=authorParts[1];
+				lastName= authorParts[0];
+				}else{
+					firstName=authorParts[0];
+				}
+				item.creators.push( {lastName:lastName.trim(), firstName:firstName.trim(), creatorType:"creator" });
 			}
 		}
-
+		
+			//add pages
+			var pages =ZU.xpath(doc, '//*[@element="format" and @qualifier="extent"]');
+			var cleanpages= pages[0].textContent.split('p.')[0];
+			if (pages.length>0) {
+					item.pages="p. 1-"+cleanpages;
+			}
+			//add type
+			item.type="Ph.D. Thesis";
+			
 		item.complete();
 
 }
